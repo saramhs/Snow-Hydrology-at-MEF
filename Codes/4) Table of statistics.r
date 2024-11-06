5ad31ec17ba38d2946c6db856efb5e2d22878e12
Sara <146585961+saramhs@users.noreply.github.com>
2024-11-06 13:04:19 -0600

    Delete Codes directory

--- Codes/4) Table of statistics.r
+++
@@ -1,9 +0,0 @@
-############################## extable of statistics for each source water and their selected tracers ##################
-library(dplyr)
-stream_tracers_summary<-stream_tracers[,-c(8,10,11)]%>%
-  group_by(name) %>%
-  summarise(across(everything(), list(mean = ~mean(.), sd = ~sd(.), median = ~median(.)), .names = "{.col}_{.fn}"))
-
-print(stream_tracers_summary)
-stream_tracers_summary<-as.data.frame(stream_tracers_summary)
-write.csv((stream_tracers_summary), "stream_tracers_summary.csv", row.names = FALSE)
